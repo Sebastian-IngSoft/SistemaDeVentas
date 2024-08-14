8f2fc1bdd7638ac86af5a41656305fdad908ef49
Sebastian-IngSoft <sebastiansarmientocas@gmail.com>
2024-08-14 12:12:38 -0500

    se agrego bootstrap en vite, se modifico estilos en la vista login

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/bootstrap.min.css',
                 'resources/js/app.js',
                 'resources/js/bootstrap.bundle.min.js',
             ],
